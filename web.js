b1e56c3883af97bdb3baff8f49ea5848205b569a
Tai Chou <chouta@gmail.com>
2013-07-11 06:10:16 +0000

    Read index.html test

--- web.js
+++ web.js
@@ -10,3 +10,10 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+app.get('index.html', function(request, response) {
+    fs.readFileSync('index.html', function(err, data){
+	if (err) throw err;
+	response.send(data.toString("utf-8");
+    })
+});
