f75095f95998fb0c2dbe91bf0ce15ad6bae406c7
Tai Chou <chouta@gmail.com>
2013-07-11 07:04:19 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -3,15 +3,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-//});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
 app.get('/',function(request, response) {
     var content;
     fs.readFileSync('./index.html', function(err, data){
@@ -19,5 +10,11 @@ app.get('/',function(request, response) {
 	content = data;
 	console.log(content);
     });
+    
     response.send(data.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
